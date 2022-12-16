9e02e88bdbd566b7ef6c07b4cc4153d0d4a1d624
tugcan-kartal <tugcankartal01@gmail.com>
2022-12-16 19:50:03 +0300

    ikinci gönderme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {BiGlobe} from 'react-icons/bi';
+import {RiUserFill,RiUserAddFill} from 'react-icons/ri';
 
 function Header() {
   return (
-    <div className='bg-brand-color h-11'>Header</div>
+    <div className='bg-brand-color'>
+      <div className='container mx-auto h-11 flex items-center justify-between'>
+
+        <a href='#'>
+          <img src="https://cdn.getir.com/marketing/Getir_Logo_1621812382342.png" className='w-11 ml-4' />
+        </a>
+
+        <nav className='flex gap-x-8 text-sm font-semibold'>
+          <a href='#' className='mr-8 flex items-center gap-x-2 text-white transition-all text-oppacity-80 hover:text-opacity-100'>
+            <BiGlobe size={20} />
+            Türkçe (TR)
+          </a>
+          <a href='#' className='mr-8 flex items-center gap-x-2 text-white transition-all text-oppacity-80 hover:text-opacity-100'>
+            <RiUserFill size={20}/>
+            Giriş Yap
+          </a>
+          <a href='#' className='mr-8 flex items-center gap-x-2 text-white transition-all text-oppacity-80 hover:text-opacity-100'>
+           <RiUserFill size={20}/>
+            Kayıt ol
+          </a>
+        </nav>
+
+      </div>
+    </div>
   )
 }
 
