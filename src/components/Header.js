b2c5e974e2f1be4d5226d419bfc1b800f77d671e
tugcan-kartal <tugcankartal01@gmail.com>
2022-12-16 13:53:04 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='bg-brand-color h-11'>Header</div>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor:{
+        "brand-color": "#5d3ebc"
+      }
+    },
+  },
+  plugins: [],
+}
